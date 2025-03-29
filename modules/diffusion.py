35dadfe5b1810b44cb8300acf2be2832dde9dc9e
user683 <1556467560@qq.com>
2025-03-29 23:18:59 +0800

    update

--- modules/diffusion.py
+++ modules/diffusion.py
@@ -283,17 +283,6 @@ def betas_from_linear_variance(steps, variance, max_beta=0.999):
 
 
 def betas_for_alpha_bar(num_diffusion_timesteps, alpha_bar, max_beta=0.999):
-    """
-    Create a beta schedule that discretizes the given alpha_t_bar function,
-    which defines the cumulative product of (1-beta) over time from t = [0,1].
-
-    :param num_diffusion_timesteps: the number of betas to produce.
-    :param alpha_bar: a lambda that takes an argument t from 0 to 1 and
-                      produces the cumulative product of (1-beta) up to that
-                      part of the diffusion process.
-    :param max_beta: the maximum beta to use; use values lower than 1 to
-                     prevent singularities.
-    """
     betas = []
     for i in range(num_diffusion_timesteps):
         t1 = i / num_diffusion_timesteps
